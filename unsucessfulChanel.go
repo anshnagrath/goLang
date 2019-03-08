60ed6a0c109fc412fbe0a105cbecae2109cb52c6
anshnagrath <anshnagrath448@gmail.com>
2019-03-08 09:43:42 +0530

    bufferChan

--- unsucessfulChanel.go
+++ unsucessfulChanel.go
@@ -3,7 +3,9 @@ import "fmt"
 func main(){
 	//if both reciever and sender are not ready then it is a deadllock or no space for sending
 c:= make(chan int)
-c <- 42
+go func(){
+	c <- 42
+}()
 fmt.Println(<- c)
 
 }
