d53c10f1be0115e606b9ff7d113a7258ee73ff21
anshnagrath <anshnagrath448@gmail.com>
2019-03-08 09:37:56 +0530

    deadlock

--- unsucessfulChanel.go
+++ unsucessfulChanel.go
@@ -1,8 +1,9 @@
 package main
-
+import "fmt"
 func main(){
 	//if both reciever and sender are not ready then it is a deadllock or no space for sending
 c:= make(chan int)
 c <- 42
+fmt.Println(<- c)
 
 }
